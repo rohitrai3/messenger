c1c1489d58e18e41f72ef91cbc5e160590cacb0b
rohitrai <raistudying@gmail.com>
2023-07-09 19:01:44 +0530

    Add contacts tab in contacts panel

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -15,6 +15,26 @@ export default function Home() {
       <div className="headline-small on-background-text">
         Who do you want to chat with?
       </div>
+      <div className="contacts-panel primary-container">
+        <div className="contacts-tabs">
+          <div className="contacts-title label-large on-secondary-container-text secondary-container">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+            </svg>
+            Contacts
+          </div>
+          <div className="add-new-title label-large on-surface-text surface">
+            Add new
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
